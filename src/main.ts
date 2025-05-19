9bfbe84340c5bf6ce44cb32735a04c87b64d03b9
Francisco Donadio <francisco.donadio@hotmail.com>
2025-05-19 20:07:36 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Star Wars Movies API')
+    .setDescription('Backend NestJS API for Star Wars Movies')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // accesible en /api
+
   await app.listen(3000);
 }
 bootstrap();
